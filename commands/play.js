e2dc6b0ae2b7391e38cc1611c321b5c90849241f
karmveerranawat <karmveerranawat@gmail.com>
2021-10-04 16:11:34 +0530

    Repo initi and base structure deployment

---
+++ commands/play.js
@@ -0,0 +1,129 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const {
+  joinVoiceChannel,
+  VoiceConnectionStatus,
+  createAudioPlayer,
+  createAudioResource,
+  AudioPlayerStatus,
+  getVoiceConnection,
+} = require("@discordjs/voice");
+const ytdl = require("ytdl-core");
+const ytSearch = require("yt-search");
+let trigger = 0;
+let queue = [];
+//player and resource initiation
+const player = createAudioPlayer();
+
+//vc join function
+function vcConnect(message) {
+  const connection = joinVoiceChannel({
+    channelId: message.member.voice.channel.id,
+    guildId: message.channel.guild.id,
+    adapterCreator: message.channel.guild.voiceAdapterCreator,
+  });
+  return connection;
+}
+
+//player starting
+function playAudio(args) {
+  const resource = createAudioResource(ytdl(args, { filter: "audio" }));
+  player.play(resource);
+}
+
+function validURL(str) {
+  var regex =
+    /(http|https):\/\/(\w+:{0,1}\w*)?(\s+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+  if (!regex.test(str)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+async function audioVerifyPlayer(args) {
+  console.log(args);
+  if (validURL(args)) {
+    playAudio(args);
+  } else {
+    const videoFinder = async (query) => {
+      const videoResult = await ytSearch(query);
+      return videoResult.videos.length > 1 ? videoResult.videos[0] : null;
+    };
+
+    const video = await videoFinder(args);
+    playAudio(video.url);
+  }
+}
+
+function addListerners(message) {
+  var connection = getVoiceConnection(message.channel.guild.id);
+  connection.on(VoiceConnectionStatus.Disconnected, async () => {
+    console.log("Disconnected");
+  });
+
+  connection.on(VoiceConnectionStatus.Destroyed, async () => {
+    console.log("Destroyed");
+  });
+
+  player.on(AudioPlayerStatus.Playing, () => {
+    console.log("The audio player has started playing!");
+  });
+
+  player.on(AudioPlayerStatus.Idle, async () => {
+    if (!queue.length) {
+      connection.destroy();
+      player.removeAllListeners();
+      player.stop();
+      trigger = 0;
+      return message.channel.send("Bot Disconnected due to empty queue");
+    }
+    console.log("player idle");
+    //music queue
+    console.log(queue);
+    //play the next song on queue
+    await audioVerifyPlayer(queue[0]);
+    message.channel.send("Now Playing " + queue[0]);
+    //change song sequence for next time
+    queue.shift();
+  });
+
+  player.on(AudioPlayerStatus.Buffering, async () => {
+    console.log("Its Buffering");
+  });
+
+  player.on("error", (e) => {
+    message.channel.send("lmao bot crashed , error log is " + e);
+  });
+}
+
+module.exports = {
+  data: new SlashCommandBuilder().setName("play").setDescription("Plays Song"),
+  async execute(message, args) {
+    if (args.length) {
+      //creating connection with vc and adding to queue
+      if (trigger == 0) {
+        var connection = vcConnect(message);
+
+        connection.on(VoiceConnectionStatus.Ready, async () => {
+          connection.subscribe(player);
+          console.log(
+            "The connection has entered the Ready state - ready to play audio!"
+          );
+          message.reply("Bot joined VC!");
+        });
+
+        //url / term checker
+        audioVerifyPlayer(args.join(" "));
+        addListerners(message);
+        trigger++;
+      } else {
+        var connection = getVoiceConnection(message.channel.guild.id);
+        queue.push(args.join(" "));
+        console.log(queue);
+        message.channel.send(
+          "song has been added to the queue, current queue is " + queue
+        );
+      }
+    }
+  },
+};
---
+++ main.js
@@ -0,0 +1,71 @@
+//global req
+const fs = require("fs");
+// Require the necessary discord.js classes
+const { Client, Collection, Intents } = require("discord.js");
+const dotenv = require("dotenv");
+
+//dotenv token initalization
+dotenv.config();
+
+//prefix
+const prefix = "%";
+
+// Create a new client instance
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+  ],
+});
+
+//Command Handler
+client.commands = new Collection();
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
+}
+
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+  console.log("Test Bot Online!");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
+
+client.on("messageCreate", async (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = client.commands.get(args.shift().toLowerCase());
+  try {
+    await command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    await message.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
+// Login to Discord with your client's token
+client.login(process.env.TOKEN);
